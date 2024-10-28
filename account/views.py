d6b7338042c67d2971f44dd4db2dd12caadacbe5
Evgenia Sologuk <108033937+Lestwa@users.noreply.github.com>
2024-10-28 23:25:25 +0400

    user_list_views

--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 def user_login(request):
@@ -82,3 +83,22 @@ def social(request):
         'version': version,
 
     }
+
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,
+                  'account/user/list.html',
+                  {'section': 'people',
+                  'users': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User,
+                             username=username, 
+                             is_active=True)
+    return render(request,
+                  'account/user/detail.html',
+                  {'section': 'people',
+                   'user': user})
