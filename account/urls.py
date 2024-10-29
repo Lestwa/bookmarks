50ec55c391a51c7e416a7e88790f58f9a217131d
Evgenia Sologuk <108033937+Lestwa@users.noreply.github.com>
2024-10-29 07:23:27 +0400

    add_account_list_template

--- account/urls.py
+++ account/urls.py
@@ -39,6 +39,8 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/<username>/', views.user_detail, name='user_detail'),
     #path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL},
          #name='logout'),
 ]
